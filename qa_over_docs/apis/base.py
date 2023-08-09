7acbe998ac26121fd85a13a0b7d71c0a9820b4db
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-08-09 11:55:34 +0800

    api class and openai base class

---
+++ qa_over_docs/apis/base.py
@@ -0,0 +1,30 @@
+from typing import List, TypedDict
+
+class ChatResponse(TypedDict):
+    relevant_source_ids: List[int]
+    answer: str
+
+class BaseAPI():
+    def __init__(self, system_instructions, user_reminder) -> None:
+        self.system_instructions = system_instructions
+        self.user_reminder = user_reminder
+
+
+    def num_tokens_from_string(self, string: str) -> int:
+        """Returns the number of tokens in a text string."""
+        num_tokens = 0
+        return num_tokens
+
+
+    def get_messages_token_length(self, messages: list[dict]):
+        """Returns the total amount of tokens in the messages list"""
+        content = [i["content"] for i in messages]
+        return self.num_tokens_from_string("\n".join(content))
+
+
+    def retrieve_response(self, question: str, relevant_docs: list[dict]) -> ChatResponse:        
+        """
+        Given a question and a list of relevant docs, returns a response with at least an text answer
+        """
+        response: ChatResponse = response
+        return response
---
+++ qa_over_docs/apis/openai.py
@@ -0,0 +1,91 @@
+from qa_over_docs.apis.base import BaseAPI, ChatResponse
+import openai, tiktoken, json
+from pprint import pprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SYSTEM_INSTRUCTIONS = '''
+Answer the user's question based on the given context.
+Return your response in JSON like this:
+
+{
+    "relevant_source_ids": [],
+    "answer": ...
+}
+
+where "relevant_source_ids" is a list of {SOURCE ID} of the sources
+relevant to the user's question. "answer" is your response to the user's
+question based on the context. For example:
+
+{
+    "relevant_source_ids": ["443350746039594436", "443350746039594128"],
+    "answer": "The answer to your question is..."
+}
+'''
+
+USER_REMINDER = '''
+Please remember to respond in JSON shown before and to use '\\n' for newlines.
+Also remember to provide the source ids of all relevant sources. Ensure you
+retrieve back ALL source ids whether the content is relevant to the user's question.
+Thank you.
+'''
+
+
+class OpenAI(BaseAPI):
+    def __init__(self) -> None:
+        super().__init__(SYSTEM_INSTRUCTIONS, USER_REMINDER)
+
+
+    def num_tokens_from_string(self, string: str) -> int:
+        encoding = tiktoken.get_encoding("cl100k_base")
+        num_tokens = len(encoding.encode(string))
+        return num_tokens
+    
+
+    def retrieve_response(self, question: str, relevant_docs: list[dict]) -> ChatResponse:
+        messages = [
+            {"role": "system", "content": SYSTEM_INSTRUCTIONS},
+            {"role": "system", "content": "CONTEXT: \n\n\n\n"},
+            {"role": "user", "content": f"{question}\n\n{USER_REMINDER}"}
+        ]
+
+        base_token_length = self.get_messages_token_length(messages)
+        max_token_length = 3700
+
+        context = ""
+
+        for doc in relevant_docs:
+            new_context = context
+            new_context += "\n\n\n" + "{SOURCE ID}: " +  str(doc["pk"]) + "\n" + doc["text"]
+
+            if self.num_tokens_from_string(new_context) > max_token_length - base_token_length:
+                break
+
+            context = new_context
+
+        messages[1]["content"] += context
+
+        pprint(messages)
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+
+        response_string = completion.choices[0].message.content.strip()
+        print(response_string)
+        
+        response: dict
+        try:
+            response = json.loads(response_string)
+            response["relevant_source_ids"] = [int(source_id) for source_id in response["relevant_source_ids"]]
+        except Exception as e:
+            print(f"{str(e)}\nCouldn't parse model's response as JSON:\n{response_string}")
+            response = {
+                "relevant_source_ids": [],
+                "answer": response_string
+            }
+        response: ChatResponse = response
+
+        return response
